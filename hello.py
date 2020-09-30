aed603fdc06348ae98854eea6aef7f5d0dde0507
HX001 <hongxu2015@gmail.com>
2020-09-30 19:19:53 -0400

    Finish Activity 2
    Finish activity 2
    1. replay the examples from chapter
    2. add email form (check 'utoronto' as key in email)

--- hello.py
+++ hello.py
@@ -1,11 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from datetime import datetime
 from flask_moment import Moment
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.fields.html5 import EmailField
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+def emailCheck(form, field):
+    # if '@' not in field.data:
+    #     raise ValidationError("Please include an '@' in an email address. \
+    #     {input} is missing an '@'.".format(input=str(field.data)))
+    if "utoronto" not in field.data:
+        field.data = "Please use your UofT email."
+
+
+
+class Form(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[DataRequired(), emailCheck])
+    submit = SubmitField('Submit')
 
 
 @app.errorhandler(404)
@@ -18,10 +40,20 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                           current_time=datetime.utcnow())
+    form = Form()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        old_email = session.get('email')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 
 @app.route('/user/<name>')
